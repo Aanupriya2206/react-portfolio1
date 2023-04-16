4cd16ff45f5a4dec18044d67fcfdf161e12030bf
Aanupriya2206 <108046953+Aanupriya2206@users.noreply.github.com>
2023-04-16 22:10:35 +0530

    commit message

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,6 +1,8 @@
 import pro1 from "../assets/codepen.png"
-import pro2 from "../assets/qrgen.png"
-import pro3 from "../assets/todo.png"
+import pro2 from "../assets/portfolio.jpg"
+import pro3 from "../assets/textutils.png"
+import pro4 from "../assets/netflix.jpg"
+import pro5 from "../assets/qrgen.png"
 const ProjectCardData=[
     {
         imgsrc:pro1,
@@ -11,15 +13,31 @@ const ProjectCardData=[
     },
     {
         imgsrc:pro2,
+        title:"Portfolio Website",
+        text:"A React-based Portfolio website that contains information about me and the projects I have made. ",
+        view:"react-portfolio1-3fal5mytj-aanupriya2206.vercel.app",
+        source:"https://github.com/Aanupriya2206/react-portfolio1.git"
+    },
+    {
+        imgsrc:pro3,
+        title:"Textutils-Text Analyzer",
+        text:"The Textutils App that I have built will allow a user to convert the text in lowercase, uppercase,remove extra spaces, count the number of words,characters and whitespace.It is a sort of text-analyzer.",
+        view:"react-textutils-a3pzof3dd-aanupriya2206.vercel.app",
+        source:"https://github.com/Aanupriya2206/react-textutils.git"
+    },
+    {
+        imgsrc:pro4,
+        title:"Netflix-Landing page",
+        text:"Netflix Landing page is a react-based applications in which iI've used React components, hooks, states and props.",
+        view:"landing-page-47tkdidws-aanupriya2206.vercel.app",
+        source:"https://github.com/Aanupriya2206/landing-page.git"
+    },
+    {
+        imgsrc:pro5,
         title:"QR Generator",
         text:"A React-based QR Generator that can generate QR's for any text or url. I have used the react-qr-code package to achieve so. For the QR code, we are going to use an npm package called react-qr-code. ",
         view:"qr-code-react-5gg5xrju5-aanupriya2206.vercel.app",
         source:"https://github.com/Aanupriya2206/QRCode-React.git"
-    },
-    {
-        imgsrc:pro3,
-        title:"To-Do List",
-        text:"The To Do App that I have built will allow a user to add a task to a list of to-do items. Once the task is added, the user will be able to delete it and update it. Hooks like useSate is used while making the To-Do List.",
     },
 ];
 
