156145d922dfa18a1b57afce960d69d2a6bf6688
Aanupriya2206 <108046953+Aanupriya2206@users.noreply.github.com>
2023-06-29 15:32:33 +0530

    commit message

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,39 +1,47 @@
-import pro1 from "../assets/codepen.png"
-import pro2 from "../assets/portfolio.jpg"
-import pro3 from "../assets/textutils.png"
-import pro4 from "../assets/netflix.jpg"
-import pro5 from "../assets/qrgen.png"
+import pro1 from "../assets/movies.png"
+import pro2 from "../assets/codepen.png"
+import pro3 from "../assets/portfolio.jpg"
+import pro4 from "../assets/textutils.png"
+import pro5 from "../assets/netflix.jpg"
+import pro6 from "../assets/qrgen.png"
 const ProjectCardData=[
     {
         imgsrc:pro1,
+        title:"Movies.com",
+        text:"A React.js based Movies reviewer website that will dispay the popular, upcoming and the top-rated movie of the recent times. It is a free platform t get the details of the movies.I've used fetch API,React Components, Hooks, States and Props to build this project.",
+        view:"https://movies-com-sigma.vercel.app/",
+        source:"https://github.com/Aanupriya2206/Movies.com.git"
+    },
+    {
+        imgsrc:pro2,
         title:"Codepen Clone",
         text:"A HTML,CSS,JavaScript based Editor to enhance your front-end web development skills. It is a free platform to practise and have hands-on HTML,CSS,JS.",
         view:"https://editor-inky.vercel.app/",
         source:"https://github.com/Aanupriya2206/react-code-editor.git"
     },
     {
-        imgsrc:pro2,
+        imgsrc:pro3,
         title:"Portfolio Website",
         text:"A React-based Portfolio website that contains information about me and the projects I have made. ",
         view:"https://react-portfolio1-navy.vercel.app/",
         source:"https://github.com/Aanupriya2206/react-portfolio1.git"
     },
     {
-        imgsrc:pro3,
+        imgsrc:pro4,
         title:"Textutils-Text Analyzer",
         text:"The Textutils App that I have built will allow a user to convert the text in lowercase, uppercase,remove extra spaces, count the number of words,characters and whitespace.It is a sort of text-analyzer.",
         view:"https://react-textutils-two.vercel.app/",
         source:"https://github.com/Aanupriya2206/react-textutils.git"
     },
     {
-        imgsrc:pro4,
+        imgsrc:pro5,
         title:"Netflix-Landing page",
         text:"Netflix Landing page is a react-based applications in which iI've used React components, hooks, states and props.",
         view:"https://landing-page-omega-amber.vercel.app/",
         source:"https://github.com/Aanupriya2206/landing-page.git"
     },
     {
-        imgsrc:pro5,
+        imgsrc:pro6,
         title:"QR Generator",
         text:"A React-based QR Generator that can generate QR's for any text or url. I have used the react-qr-code package to achieve so. For the QR code, we are going to use an npm package called react-qr-code. ",
         view:"https://qr-code-react-iota.vercel.app/",
